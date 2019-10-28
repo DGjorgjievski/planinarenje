ce65b63a9683998fb887b25683a4acc342bdb66d
DGjorgjievski <gjorgjievskidavor@yahoo.com>
2019-10-28 15:33:04 +0100

    fixbug1

--- planinarenje/src/Login/LoginLayout.js
+++ planinarenje/src/Login/LoginLayout.js
@@ -3,22 +3,39 @@ import React from 'react';
 const Login = (  ) => {
     return(
         <div>
-            <form>
-                <div className="form-group">
-                    <label htmlFor="exampleInputEmail1">Email address</label>
-                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
-                </div>   
-                <div className="form-group">
-                    <label htmlFor="exampleInputPasshtmlFord1">Password</label>
-                    <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
-                </div> 
-                <div className="form-group form-check">
-                    <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-                    <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
+            <div class="login-reg-panel">
+                <div class="login-info-box">
+                <h2>Have an account?</h2>
+                <p>Lorem ipsum dolor sit amet</p>
+                <label id="label-register" for="log-reg-show">Login</label>
+                <input type="radio" name="active-log-panel" id="log-reg-show"  checked="checked"/>
+            </div>
+                                
+            <div class="register-info-box">
+                <h2>Don't have an account?</h2>
+                <p>Lorem ipsum dolor sit amet</p>
+                <label id="label-login" for="log-login-show">Register</label>
+                <input type="radio" name="active-log-panel" id="log-login-show"/>
+            </div>
+                                
+            <div class="white-panel">
+                <div class="login-show">
+                    <h2>LOGIN</h2>
+                    <input type="text" placeholder="Email"/>
+                    <input type="password" placeholder="Password"/>
+                    <input type="button" value="Login"/>
+                    <a href="">Forgot password?</a>
                 </div>
-                <button type="submit" className="btn btn-primary">Submit</button>
-            </form>        
-        </div>
+                <div class="register-show">
+                    <h2>REGISTER</h2>
+                    <input type="text" placeholder="Email"/>
+                    <input type="password" placeholder="Password"/>
+                    <input type="password" placeholder="Confirm Password"/>
+                    <input type="button" value="Register"/>
+                </div>
+            </div>
+	    </div>       
+    </div>
     ) 
 };
 export default Login;
