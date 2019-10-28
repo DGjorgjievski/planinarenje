f8008fbc702dc229d3c0a6141de11ed3f8495bdd
Frose <gicevafrose@gmail.com>
2019-10-28 16:07:48 +0100

    Added LanginPageText

--- planinarenje/src/LandingPage/LandingPage.js
+++ planinarenje/src/LandingPage/LandingPage.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import LandingPageMenu from '../components/LandingPageMenu'
+import LandingPageText from '../components/LandingPageText'
+
 
 function LandingPage(){
     return(
         <div>
             <LandingPageMenu />
+
+            <LandingPageText/>
         </div>
     )
 }
