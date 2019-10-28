d783ac073f56debd9cb53170628ad019207ec803
Brsjak <brsjakproductions@gmail.com>
2019-10-28 13:12:10 +0100

    LandingPageChanges

--- planinarenje/src/LandingPage/LandingPage.js
+++ planinarenje/src/LandingPage/LandingPage.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import LandingPageMenu from './components/LandingPageMenu'
 
 function LandingPage(){
     return(
         <div>
             <h1>Hello Frose!</h1>
             <h3>Hello Again Frose!</h3>
+            <LandingPageMenu />
         </div>
     )
 }
