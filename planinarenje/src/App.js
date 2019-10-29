296ac6229db6cef329cbf8640ad0dd583d586927
Frose <gicevafrose@gmail.com>
2019-10-29 16:48:22 +0100

    UpdatedNastaniFrose

--- planinarenje/src/App.js
+++ planinarenje/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
+import Nastani from './Nastani/Nastani';
 
 
 
 function App() {
   return (
     <div className="App">       
-        
+      <Nastani></Nastani>
     </div>
   );
 }
