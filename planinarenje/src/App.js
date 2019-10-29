bfe80ce8825f2060962f7096759ef9b351a5cdf5
Brsjak <brsjakproductions@gmail.com>
2019-10-29 15:54:45 +0100

    RESOLVED CONFLICTS APP.JS REMOVED MAIN COMPONENT

--- planinarenje/src/App.js
+++ planinarenje/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
 
-import LandingPage from './LandingPage/LandingPage'
+
 
 function App() {
   return (
     <div className="App">       
-        <LandingPage />
+        
     </div>
   );
 }
