6d955539479fe35883c8c47679a2c95cd47d59d8
DGjorgjievski <gjorgjievskidavor@yahoo.com>
2019-10-30 16:45:25 +0100

    Auth is nearly done

--- planinarenje/src/App.js
+++ planinarenje/src/App.js
@@ -1,14 +1,25 @@
-import React from 'react';
-import './App.css';
-import SignUp from './Authentication/SignUp/SignUp';
-import LandingPage from './LandingPage/LandingPage'
+import React from "react";
+import "./App.css";
+import SignUp from "./Authentication/SignUp/SignUp";
+import SignIn from "./Authentication/SignIn/SignIn";
+import LandingPage from "./LandingPage/LandingPage";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">       
-
-        <SignUp  />
-    </div>
+    <Router>
+      <div className="App">
+        <Route path="/SignIn" component={SignIn}></Route>
+        <Route path="/SignUp" component={SignUp}></Route>
+      </div>
+    </Router>
   );
 }
 
