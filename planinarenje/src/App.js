2f0a5de1a11d1c0f779ac5459e0d953a986e0456
DGjorgjievski <gjorgjievskidavor@yahoo.com>
2019-10-31 00:23:42 +0100

    pull

--- planinarenje/src/App.js
+++ planinarenje/src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "./App.css";
-<<<<<<< HEAD
 import SignUp from "./Authentication/SignUp/SignUp";
 import SignIn from "./Authentication/SignIn/SignIn";
 import LandingPage from "./LandingPage/LandingPage";
@@ -14,33 +13,13 @@ import {
   useRouteMatch,
   useParams
 } from "react-router-dom";
-=======
->>>>>>> master
 import PlaninarskiKlubovi from "./PlaninarskiKlubovi/PlaninarskiKlubovi";
 
 function App() {
   return (
-<<<<<<< HEAD
-    <Router>
-      <div className="App">
-<<<<<<< HEAD
-=======
-        <Route path="/MainPage" component={MainPage}></Route>
->>>>>>> a498082093b5abbe108c6bc5a5dd6917adc22595
-        <Route path="/LandingPage" component={LandingPage}></Route>
-        <Route path="/SignIn" component={SignIn}></Route>
-        <Route path="/SignUp" component={SignUp}></Route>
-        <Route
-          path="/PlaninarskiKlubovi"
-          component={PlaninarskiKlubovi}
-        ></Route>
-      </div>
-    </Router>
-=======
     <div className="App">
       <PlaninarskiKlubovi></PlaninarskiKlubovi>
     </div>
->>>>>>> master
   );
 }
 
