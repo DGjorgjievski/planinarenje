a64aad81147176ba9ad1daede31c135b4ec1092e
Brsjak <brsjakproductions@gmail.com>
2019-10-30 14:26:50 +0100

    removed preused components

--- planinarenje/src/App.js
+++ planinarenje/src/App.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import './App.css';
-import Nastani from './Nastani/Nastani';
-import LandingPage from './LandingPage/LandingPage';
-import LandingPageText from './components/LandingPageText';
 
 
 
 function App() {
   return (
     <div className="App">       
-     <Nastani></Nastani>
+     
     </div>
   );
 }
