bf75521f77f6a9b75b5f515b1e24aabba447d2c3
DGjorgjievski <gjorgjievskidavor@yahoo.com>
2019-10-30 23:25:51 +0100

    bug

--- planinarenje/src/App.js
+++ planinarenje/src/App.js
@@ -1,9 +1,10 @@
-<<<<<<< HEAD
 import React from "react";
 import "./App.css";
 import SignUp from "./Authentication/SignUp/SignUp";
 import SignIn from "./Authentication/SignIn/SignIn";
 import LandingPage from "./LandingPage/LandingPage";
+import MainPage from "./MainPage/MainPage";
+
 import {
   BrowserRouter as Router,
   Switch,
@@ -17,27 +18,12 @@ function App() {
   return (
     <Router>
       <div className="App">
+        <MainPage></MainPage>
+        <Route path="/LandingPage" component={LandingPage}></Route>
         <Route path="/SignIn" component={SignIn}></Route>
         <Route path="/SignUp" component={SignUp}></Route>
       </div>
     </Router>
-=======
-import React from 'react';
-import './App.css';
-
-
-import SignUp from './Authentication/SignUp/SignUp';
-import Button from '@material-ui/core/Button';
-import LandingPage from './LandingPage/LandingPage'
-
-
-
-function App() {
-  return (
-    <div className="App">       
-
-    </div>
->>>>>>> 54be4507b372ebc5c6d8fc94c6f50b0f608a3f31
   );
 }
 
--- planinarenje/src/components/LandingPageText.js
+++ planinarenje/src/components/LandingPageText.js
@@ -1,41 +1,44 @@
-import React from 'react';
+import React from "react";
 
-const paragrafStyle={
+function LandingPageText() {
+  return <div></div>;
 
-    color:'white',
-    margin:'0 auto',
-    width:'46%',
-    paddingTop:"50px",
-    paddingBottom:"50px"
+  const paragrafStyle = {
+    color: "snow",
+    margin: "0 auto",
+    width: "46%",
+    paddingTop: "50px",
+    paddingBottom: "50px",
+    fontWeight: "bold"
+  };
+  const naslovStyle = {
+    color: "snow",
+    margin: "0 auto",
+    width: "46%",
+    paddingTop: "200px"
+  };
+  const kopceStyle = {
+    margin: "0 auto",
+    color: "snow"
+  };
 
+  function LandingPageText() {
+    return (
+      <div>
+        <h1 className="naslov" style={naslovStyle}>
+          ДОБРЕДОЈДОВТЕ НА ПЛАНИНАРЕЊЕ
+        </h1>
+        <p className="paragraf" style={paragrafStyle}>
+          Ви ја претставуваме нашата страна каде на забавен начин можете да
+          пребарувате низ најновите содржини,настани,информации и друштва
+          поврзани со планинарењето низ Македонија.За да бидете во тек следете
+          ја нашата страна и креирајте свој профил.
+        </p>
+        <button type="button" className="btn btn-primary" style={kopceStyle}>
+          Влез
+        </button>
+      </div>
+    );
+  }
 }
-const naslovStyle={
-    color:'white',
-    margin:'0 auto',
-    width:'46%',
-    paddingTop:"200px"
-    
-}
-const kopceStyle={
-    margin:'0 auto',
-    
-}
-
-function LandingPageText(){
-
-    return(
-        <div>
-            <h1 className="naslov" style={naslovStyle}>ДОБРЕДОЈДОВТЕ НА ПЛАНИНАРЕЊЕ</h1>
-            <p className="paragraf" style={paragrafStyle}>
-            Ви ја претставуваме нашата страна каде на забавен начин можете да пребарувате низ најновите 
-        содржини,настани,информации и друштва поврзани со планинарењето низ Македонија.За да бидете во тек следете ја нашата 
-        страна и креирајте свој профил.
-            </p>
-            <button type="button" className="btn btn-outline-light" style={kopceStyle}>Влез</button>
-        </div>
-    )
-
-
-}
-
-export default LandingPageText
+export default LandingPageText;
