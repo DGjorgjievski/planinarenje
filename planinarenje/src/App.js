3c4404bf6f3e2416f17f6cc1f2f19c63fd44ed1b
Brsjak <brsjakproductions@gmail.com>
2019-10-31 13:33:06 +0100

    MainHeader and MainCarousel divided

--- planinarenje/src/App.js
+++ planinarenje/src/App.js
@@ -18,15 +18,7 @@ import Authentication from "./Authentication/Authentication";
 
 function App() {
   return (
-    <Router>
-      <Route></Route>
-      <Route path="/LandingPage" component={LandingPage}></Route>
-      <Route path="/MainPage" component={MainPage}></Route>
-      <Route path="/Nastani" component={Nastani}></Route>
-      <Route path="/Authentication" component={Authentication}></Route>
-      <Route path="/NastaniCards" component={NastaniCards}></Route>
-      <Route path="/PlaninarskiKlubovi" component={PlaninarskiKlubovi}></Route>
-    </Router>
+    <MainPage />
   );
 }
 
--- planinarenje/src/MainPage/MainPage.js
+++ planinarenje/src/MainPage/MainPage.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Header from '../Header/Header';
+import {MainCarousel} from '../components';
+import CardsContainer from '../components/CardsContainer';
 
 function MainPage(){
     return(
         <div>
             <Header />
+            <MainCarousel />
+            <CardsContainer />
         </div>
     )
 }
