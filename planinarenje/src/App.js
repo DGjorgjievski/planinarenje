eae074a9df6fbee9ba01897901edf4016b50e728
Brsjak <brsjakproductions@gmail.com>
2019-10-31 09:30:26 +0100

    smth

--- planinarenje/src/App.js
+++ planinarenje/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
-
+import MainPage from './MainPage/MainPage'
 
 
 function App() {
-  return (
+  return ( 
     <div className="App">       
-
+        <MainPage />
     </div>
   );
 }
