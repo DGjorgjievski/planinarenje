1e9cc5858d3b34ee3265c9ed14c3070c0175fe3f
Filip Brsjak <brsjakproductions@gmail.com>
2019-10-31 09:43:26 +0100

    Update App.js
    Removed Favor's FuckUps

--- planinarenje/src/App.js
+++ planinarenje/src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "./App.css";
-import PlaninarskiKlubovi from "./PlaninarskiKlubovi/PlaninarskiKlubovi";
+
 
 function App() {
   return (
     <div className="App">
-      <PlaninarskiKlubovi></PlaninarskiKlubovi>
+      
     </div>
   );
 }
