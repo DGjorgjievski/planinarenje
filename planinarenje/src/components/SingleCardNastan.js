c088fbb5a3ceece19267064662dfe54d56b8290f
Brsjak <brsjakproductions@gmail.com>
2019-11-01 04:38:23 +0100

    Cards Implemented MaterialUI

--- planinarenje/src/components/SingleCardNastan.js
+++ planinarenje/src/components/SingleCardNastan.js
@@ -1,38 +1,138 @@
-import React, { useState } from "react";
+import React from 'react';
 import { Link, NavLink } from "react-router-dom";
+import { makeStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Collapse from '@material-ui/core/Collapse';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import { red } from '@material-ui/core/colors';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ShareIcon from '@material-ui/icons/Share';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import Button from '@material-ui/core/Button';
+import { shadows } from '@material-ui/system';
+import Logo from '../SLIKI/logo25.png';
+import AssignmentIcon from '@material-ui/icons/Assignment';
 
-const SingleCardNastan = () => {
-  const btnStyle = {
-    float: "right",
-    marginTop: "20px"
+const useStyles = makeStyles(theme => ({
+  card: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 0,
+    paddingTop: '56.25%', // 16:9
+  },
+  expand: {
+    transform: 'rotate(0deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)',
+  },
+  avatar: {
+    margin:10,
+  },
+  button: {
+    margin: theme.spacing(1),
+  },
+  input: {
+    display: 'none',
+  },
+}));
+
+export default function RecipeReviewCard() {
+  const classes = useStyles();
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
   };
-  const slikaStyle = {
-    width: "100%",
-    maxHeight: 250,
-    height: "100%"
-  };
+
   return (
-    <div className="card">
-      <img
-        className="card-img-top"
-        src={require("../SLIKI/s1.jpg")}
-        style={slikaStyle}
+    <Card className={classes.card}>
+      <CardHeader
+        avatar={
+          <Avatar aria-label="recipe" className={classes.avatar}>
+            <AssignmentIcon />
+          </Avatar>
+        }
+        action={
+          <IconButton aria-label="settings">
+            <MoreVertIcon />
+          </IconButton>
+        }
+        title="Љуботен 2499"
+        subheader="1.11.2019"
       />
-      <div className="card-body">
-        <h5 className="card-title">Љуботен 2499</h5>
-        <p className="card-text">
+      <CardMedia
+        className={classes.media}
+        image={require("../SLIKI/plan5.jpg")}
+        title="Слика5"
+      />
+      <CardContent>
+        <Typography variant="body2" color="textSecondary" component="p">
           Атрактивноста на турата е во врвот, кој има форма на пирамида. Љуботен
           важи за малку суров врв поради тоа што претставува еден вид
-          "пристаниште" на Шара каде се прекршуваат сите ветришта.{" "}
-        </p>
+          "пристаниште" на Шара каде се прекршуваат сите ветришта.
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <IconButton aria-label="add to favorites">
+          <FavoriteIcon />
+        </IconButton>
+        <IconButton aria-label="share">
+          <ShareIcon />
+        </IconButton>
+        <IconButton
+          className={clsx(classes.expand, {
+            [classes.expandOpen]: expanded,
+          })}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
         <Link to="infoNastan">
-          <button className="btn btn-info" style={btnStyle}>
-            Повеќе
-          </button>
+          <Button variant="contained" className={classes.button} color="secondary" >ПОВЕЌЕ</Button>
         </Link>
-      </div>
-    </div>
+        
+        </IconButton>
+      </CardActions>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+          <Typography paragraph>Method:</Typography>
+          <Typography paragraph>
+            Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10
+            minutes.
+          </Typography>
+          <Typography paragraph>
+            Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over medium-high
+            heat. Add chicken, shrimp and chorizo, and cook, stirring occasionally until lightly
+            browned, 6 to 8 minutes. Transfer shrimp to a large plate and set aside, leaving chicken
+            and chorizo in the pan. Add pimentón, bay leaves, garlic, tomatoes, onion, salt and
+            pepper, and cook, stirring often until thickened and fragrant, about 10 minutes. Add
+            saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
+          </Typography>
+          <Typography paragraph>
+            Add rice and stir very gently to distribute. Top with artichokes and peppers, and cook
+            without stirring, until most of the liquid is absorbed, 15 to 18 minutes. Reduce heat to
+            medium-low, add reserved shrimp and mussels, tucking them down into the rice, and cook
+            again without stirring, until mussels have opened and rice is just tender, 5 to 7
+            minutes more. (Discard any mussels that don’t open.)
+          </Typography>
+          <Typography>
+            Set aside off of the heat to let rest for 10 minutes, and then serve.
+          </Typography>
+        </CardContent>
+      </Collapse>
+    </Card>
   );
-};
-
-export default SingleCardNastan;
+}
